2594e21411079f487176abf1cb16ec167a69ab30
∅ח א כ ד ה יד∅ <cubedimension@protonmail.com>
2023-11-01 17:26:15 +0530

    Update PYBTC-linux.py

--- PYBTC-linux.py
+++ PYBTC-linux.py
@@ -3,99 +3,72 @@ import blockcypher
 import json
 import webbrowser
 
-# Linux-friendly file paths
-api_json_path = 'api.json'
-private_key_json_path = 'private_key.json'
-wallet_json_path = 'wallet.json'
-info_png_path = 'info.png'
-generate_png_path = 'generate.png'
-send_png_path = 'send.png'
-wallet_png_path = 'wallet.png'
-key_png_path = 'key.png'
-web_png_path = 'web.png'
-
 # Load the API key from api.json
-with open(api_json_path, 'r') as f:
+with open('api.json', 'r') as f:
     api_data = json.load(f)
     API_KEY = api_data['api_key']
 
-# Load private key
-with open(private_key_json_path, 'r') as f:
+# Load the private key
+with open('private_key.json', 'r') as f:
     private_key_data = json.load(f)
     private_key = private_key_data['private_key']
 
-def run_file_1():
-    # Generate a new random private key
+# Function to generate a new Bitcoin address
+def generate_new_address():
     new_key = blockcypher.generate_new_address('btc', api_key=API_KEY)['private']
-
-    # Get the corresponding Bitcoin address
     new_address = blockcypher.generate_new_address('btc', api_key=API_KEY)['address']
-
-    # Update the label text with the Bitcoin address
     label.config(text="Your BTC Address: " + new_address)
 
-def run_file_2():
-    # Create a new window for sending BTC
+# Function to send BTC
+def send_btc():
     send_window = tk.Toplevel(pybtc)
     send_window.title("Send BTC")
 
-    # Create a label and entry for entering the address
     address_label = tk.Label(send_window, text="To Address:")
     address_label.pack()
     address_entry = tk.Entry(send_window)
     address_entry.pack()
 
-    # Create a label and entry for entering the amount
     amount_label = tk.Label(send_window, text="Amount:")
     amount_label.pack()
     amount_entry = tk.Entry(send_window)
     amount_entry.pack()
 
-    # Function to handle the send button click
     def send_button_click():
-        # Get the address and amount from the entries
         to_address = address_entry.get()
         amount = float(amount_entry.get())
 
-        # Load the wallet address and private key from wallet.json
-        with open(wallet_json_path, 'r') as f:
+        with open('wallet.json', 'r') as f:
             wallet_data = json.load(f)
             address = wallet_data['address']
 
-        # Create the transaction
         inputs = [{'address': address}]
-        outputs = [{'address': to_address, 'value': int(amount * 100000000)}
+        outputs = [{'address': to_address, 'value': int(amount * 100000000)}]
 
         unsigned_tx = blockcypher.create_unsigned_tx(inputs=inputs, outputs=outputs, coin_symbol='btc')
         signed_tx = blockcypher.sign_tx(unsigned_tx, wifs=private_key, coin_symbol='btc')
         broadcasted_tx = blockcypher.broadcast_signed_transaction(signed_tx, coin_symbol='btc')
 
-        # Update the label text with the transaction status
         result_label.config(text="Transaction sent!")
 
-    # Create a send button
     send_button = tk.Button(send_window, text="Send", command=send_button_click)
     send_button.pack()
 
-    # Create a label to display the transaction status
     result_label = tk.Label(send_window, text="")
     result_label.pack()
 
-def run_file_3():
-    with open(wallet_json_path, 'r') as f:
+# Function to display balance and transactions
+def display_balance_and_transactions():
+    with open('wallet.json', 'r') as f:
         wallet_data = json.load(f)
         address = wallet_data['address']
 
     balance = blockcypher.get_total_balance(address, coin_symbol='btc')
-
-    # Update the label text with the BTC balance
     label.config(text="Your BTC Balance: {} BTC".format(balance / 100000000))
 
-    # Create a new window for displaying transactions
     transaction_window = tk.Toplevel(pybtc)
     transaction_window.title("Transaction Details")
 
-    # Create a text widget with scrollbar for displaying transaction details
     text_widget = tk.Text(transaction_window, height=10, width=50)
     text_widget.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
 
@@ -105,8 +78,8 @@ def run_file_3():
     text_widget.config(yscrollcommand=scrollbar.set)
     scrollbar.config(command=text_widget.yview)
 
-    # Retrieve and display transaction details
     transactions = blockcypher.get_address_details(address, coin_symbol='btc')['txrefs']
+
     if transactions:
         for tx in transactions:
             text_widget.insert(tk.END, "Transaction Hash: {}\n".format(tx['tx_hash']))
@@ -118,49 +91,38 @@ def run_file_3():
 
     text_widget.config(state=tk.DISABLED)
 
-def run_file_4():
-    # Open a web page in the default browser
-    webbrowser.open("https://www.blockcypher.com/")
-
-def run_file_5():
-    # Open the browser with the specified URL
-    webbrowser.open("https://technicalhayden.github.io/pybtc")
+# Function to open a web page
+def open_webpage(url):
+    webbrowser.open(url)
 
 # Create the main window
 pybtc = tk.Tk()
-
-# Set the window title
 pybtc.title("PY BTC WALLET")
 
-# Set the window icon
-icon = tk.PhotoImage(file=info_png_path)
-pybtc.iconphoto(False, icon)
-pybtc.geometry("600x100")
+# Load images for buttons
+generate_image = tk.PhotoImage(file="generate.png")
+send_image = tk.PhotoImage(file="send.png")
+wallet_image = tk.PhotoImage(file="wallet.png")
+key_image = tk.PhotoImage(file="key.png")
+web_image = tk.PhotoImage(file="web.png")
 
 # Create a frame to hold the buttons and label
 frame = tk.Frame(pybtc)
 frame.pack()
 
-# Load the images
-imagee = tk.PhotoImage(file=generate_png_path)
-imager = tk.PhotoImage(file=send_png_path)
-imaget = tk.PhotoImage(file=wallet_png_path)
-imagetw = tk.PhotoImage(file=key_png_path)
-imagepw = tk.PhotoImage(file=web_png_path)
-
-# Create the buttons with the images
-button1 = tk.Button(frame, image=imagee, command=run_file_1)
-button2 = tk.Button(frame, image=imager, command=run_file_2)
-button3 = tk.Button(frame, image=imaget, command=run_file_3)
-button4 = tk.Button(frame, image=imagetw, command=run_file_4)
-button5 = tk.Button(frame, image=imagepw, command=run_file_5)
+# Create and configure buttons with images
+generate_button = tk.Button(frame, image=generate_image, command=generate_new_address)
+send_button = tk.Button(frame, image=send_image, command=send_btc)
+wallet_button = tk.Button(frame, image=wallet_image, command=display_balance_and_transactions)
+key_button = tk.Button(frame, image=key_image, command=lambda: open_webpage("https://www.blockcypher.com/"))
+web_button = tk.Button(frame, image=web_image, command=lambda: open_webpage("https://haydenbanz.github.io/pybtc"))
 
 # Pack the buttons horizontally in the frame
-button1.pack(side=tk.LEFT)
-button2.pack(side=tk.LEFT)
-button3.pack(side=tk.LEFT)
-button4.pack(side=tk.LEFT)
-button5.pack(side=tk.LEFT)
+generate_button.pack(side=tk.LEFT)
+send_button.pack(side=tk.LEFT)
+wallet_button.pack(side=tk.LEFT)
+key_button.pack(side=tk.LEFT)
+web_button.pack(side=tk.LEFT)
 
 # Create a label to display the BTC address
 label = tk.Label(pybtc, text="Your BTC Address: ")
